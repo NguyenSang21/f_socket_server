edbd1c1bad4144b0fd40f421ed3da00b3c1a9490
Nguyen Sang <nguyentansang1996@gmail.com>
2019-09-29 21:47:21 +0700

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const udp = require('dgram');
 const app = express()
 const port = 2019
+// other package
 const fs = require('fs')
+// core - socket.io
 const server = require('http').Server(app);
-const server_download = udp.createSocket('udp4');
 const io = require('socket.io')(server);
+// core - UDP server 
+const udp = require('dgram');
+const server_download = udp.createSocket('udp4');
 
+/* ======================== SERVER - SOCKET.IO ========================= */
 let files = {}
 let struct = {
   name: null,
@@ -16,6 +20,7 @@ let struct = {
   slice: 0,
 }
 
+// listening port 2019
 server.listen(port, () => console.log(`App listening on port ${port}!`));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
@@ -24,12 +29,15 @@ io.on('connection', (socket) => {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event',  (data) => {
     console.log(data);
+
   });
+  socket
   // set 10 second - send data info to master server
   setInterval( () => {
     socket.emit('receive_file_info', {data: getAllFilesFromFolder()})
   }, 10000)
 
+  // listen client upload a file
   socket.on('upload-file', (data) => {
     if (!files[data.name]) {
       files[data.name] = Object.assign({}, struct, data);
@@ -57,22 +65,41 @@ io.on('connection', (socket) => {
       });
     }
   })
+  //
+  socket.emit("message", "connected")
+  socket.on('message', (data) => {
+    console.log(data);
+    // var address = srv.address();
+    // var client = dgram.createSocket("udp4");
+    // var message = new Buffer(data);
+    // client.send(message, 0, message.length, address.port, address.address, function(err, bytes) {
+    //   client.close();
+    // });
+  });
 });
 
-/* dowload using udp*/
-server_download.on('file_download', (stream, name, callback) => {
-  //== Do stuff to find your file
-  callback({
-    name : "filename",
-    size : 500
-  });
 
-  var MyFileStream = fs.createReadStream(name);
-  MyFileStream.pipe(stream);
+
+/*===========================Initialize a UDP server=============================*/
+server_download.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  socket.sockets.emit('udp message', msg);
+});
+
+server_download.on('listening', (name) => {
+  console.log("udp listening")
+  //== Do stuff to find your file  
+  fs.readFile('./public/'+ name, (err, data) => {
+    if(err) return err
+    server_download.emit("send_file_download", {buffer: data})
+    console.log('send file to client ...');
+  })
+
 })
 
-server_download.bind(2021)
+server_download.bind(7788)
 
+/*=========================== COMMON FUNCTION==============================*/
 const getAllFilesFromFolder = () => {
   let results = []
   const dir = './public/'
